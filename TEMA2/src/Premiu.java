f43ee10c87f4d049c75ce8ac534ebdfb4329f13a
DobreaMariusDorian10 <127296810+DobreaMariusDorian10@users.noreply.github.com>
2023-03-17 18:13:22 +0200

    Delete TEMA2/src directory

--- TEMA2/src/Premiu.java
+++
@@ -1,9 +0,0 @@
-public class Premiu {
-    String nume;
-    int an;
-
-    public Premiu(String nume, int an) {
-        this.nume = nume;
-        this.an=an;
-    }
-}
--- TEMA2/src/Studio.java
+++
@@ -1,8 +0,0 @@
-public class Studio {
-    String nume;
-    Film[ ] filme = new Film[5];
-    public Studio(String nume,Film[ ] filme){
-        this.nume=nume;
-        this.filme=filme;
-    }
-}
